79ebe5b6939faca8ba30e63240652c36d9306c26
MacleonDev <macpdleonr@gmail.com>
2025-11-10 01:35:11 -0500

    Update helloworld.go

--- Basic/helloworld.go
+++ Basic/helloworld.go
@@ -2,5 +2,19 @@ package main
 import "fmt"
 
 func main() {
+	
 	fmt.Println("Hello, World!")
+
+	var name string  = "Mac"
+
+	fmt.Printf("Hello, %s!\n", name)
+
+	var lastname = "Testing"
+
+	fmt.Printf("Hello, %s %s!\n", name, lastName)
+
+	petname := "Max"
+
+	fmt.Printf("Hello, %s!\n", petname)
 	}
+
